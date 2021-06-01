442d7a41e9f64738790f4e109add9b3d9828fb39
Markus Johansson <markus@obviuse.se>
2021-06-01 14:19:49 +0200

    #57 Solved by checking users and groups start nodes

--- Our.Umbraco.TheDashboard/Security/UserToNodeAccessService.cs
+++ Our.Umbraco.TheDashboard/Security/UserToNodeAccessService.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using Our.Umbraco.TheDashboard.Models;
 using Umbraco.Core.Models.Membership;
 using Umbraco.Core.Services;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Migrations.Upgrade.V_7_12_0;
+using Umbraco.Core.Models;
+using Umbraco.Web.Actions;
 
 namespace Our.Umbraco.TheDashboard.Security
 {
@@ -11,8 +16,14 @@ namespace Our.Umbraco.TheDashboard.Security
     {
         private EntityPermissionCollection _permissions;
 
+        /// <summary>
+        /// Stores the users start nodes
+        /// </summary>
+        private int[] _userStartNodes;
+
         internal UserToNodeAccessHelper(IUser currentUser, IUserService userService, IEnumerable<IUmbracoNodeWithPermissions> nodes)
         {
+            _userStartNodes = currentUser.CalculateContentStartNodeIds(Current.Services.EntityService);
             _permissions = userService.GetPermissions(currentUser, AllNodeIdsFromPath(nodes));
         }
 
@@ -37,12 +48,49 @@ namespace Our.Umbraco.TheDashboard.Security
             // for a given node but Denied for one of it's parents.
 
             // Parse path and reverse to start from the current node.
-            var nodeIds = node.NodePath.Split(',').Reverse().Select(x=>int.Parse(x));
+            var nodeIds = node.NodePath
+                .Split(',')
+                .Reverse()
+                .Select(x => int.Parse(x))
+                .Where(x => x > 0) // exlude -1 as this is the root and has no permissions.
+                .ToList()
+                ;
 
-            // Loop over each ancestor and return false if permissions are denied.
+            // Compare usersStart node(s) (if any) against the path, if not found in start nodes access should be denied
+            if (_userStartNodes.Length > 1)
+            {
+                // Users with access to "all" will have a startNode with -1
+                if (_userStartNodes.Length > 1 || _userStartNodes[0] != Constants.System.Root)
+                {
+                    // At this stage we know that the user has start nodes assigned.
+
+                    // Compare ids in the path against ids in _startNodes.
+                    // At lest one of the ids in _userStartNodes must be present in the path
+                    bool found = false;
+
+                    foreach (var startNodeId in _userStartNodes)
+                    {
+                        var inPath = nodeIds.Contains(startNodeId);
+                        if (inPath)
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    // If none of the start node ids was found in the path we should 
+                    // not allow access.
+                    if(!found)
+                        return false;
+
+                }
+
+            }
+
+            // Look for permissions on the current node and it's anscestors.
             foreach (var id in nodeIds)
             {
-                if(id > 0 && !HasPermissions(_permissions,id))
+                if(!HasPermissions(_permissions,id))
                     return false;
             }
 
@@ -55,7 +103,10 @@ namespace Our.Umbraco.TheDashboard.Security
             var forNode = permissionResults.Where(x => x.EntityId == nodeId).ToList();
             var permissions = forNode.SelectMany(x => x.AssignedPermissions).ToList();
 
-            return permissions.Contains("F");
+            // Getting the letter for the "Browse"-permissions (probably "F"), using Umbraco.Web/Actions/ActionBrowse.cs
+            var browseNodeAction = ActionBrowse.ActionLetter.ToString();
+
+            return permissions.Contains(browseNodeAction);
         }
 
     }
