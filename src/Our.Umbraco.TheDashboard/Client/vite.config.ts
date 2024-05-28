0dbbadd66234915acda830f8ca8d2002f5ac735d
Markus Johansson <markus@obviuse.se>
2024-05-28 16:33:01 +0200

    Worked on build

--- src/Our.Umbraco.TheDashboard/Client/vite.config.ts
+++ src/Our.Umbraco.TheDashboard/Client/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => {
+
+  const buildMode = process.env.build_mode?.trim();
+
+  console.log('vite.config.ts | env.buildMode',buildMode);
+
+  return {
     build: {
         lib: {
             entry: "src/entry.ts",
@@ -8,9 +14,10 @@ export default defineConfig({
             name : "TheDashboard.Umbraco"
         },
         outDir: "../wwwroot/App_Plugins/Our.Umbraco.TheDashboard/dist",
-        sourcemap: true,
+        sourcemap: buildMode == 'development' ? true : false,
         rollupOptions: {
             external: [/^@umbraco/]
         },
     }
+  }
 });
