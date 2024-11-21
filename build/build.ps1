b553edff99b7ebce9e82101197a541e9832d5aa3
Markus Johansson <markus@obviuse.se>
2024-11-21 15:22:56 +0100

    adjust verions

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,4 @@
-param ($version = "15.0.0",$versionSuffix = "")
+param ($version = "15.0.1",$versionSuffix = "")
 # Params
 # version = major.minor.patch
 # versionSuffix = eg -beta1, -rc1 (should include leading -). Leave empty string if not a pre-release.
